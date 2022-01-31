a1342a7f0f8a6807177327ad9377dca0129ae421
persefoni <ppaouri@gmail.com>
2022-01-31 20:34:12 +0200

    Add project files.

---
+++ BankAccount/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace myApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("Persefoni", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            account.MakeDeposit(150, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.GetAccountHistory());
+
+        }
+    }
+
+}
---
+++ BankAccount/Transaction.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace myApplication
+{
+    internal class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+
+        }
+    }
+}
